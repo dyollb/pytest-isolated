b243ddb01b64b251cfc99fc890346487933b4bb7
Bryn Lloyd <12702862+dyollb@users.noreply.github.com>
2026-02-12 21:59:23 +0100

    bump version

--- src/pytest_isolated/__init__.py
+++ src/pytest_isolated/__init__.py
@@ -1,3 +1,3 @@
 """pytest-isolated: Run pytest tests in isolated subprocesses."""
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,1 @@
 """Tests for pytest-isolated plugin."""
-
-__version__ = "0.1.0"
